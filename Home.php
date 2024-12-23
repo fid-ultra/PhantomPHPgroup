977105689a238f56d10fc87dddc1a659b4febd93
fid-ultra <fahadsunna2002@gmail.com>
2024-12-23 03:11:34 +0300

    test last night

--- Home.php
+++ Home.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <style>
+    p{
+        display:inline-block;
+        color:darkblue;
+        text-align: left;
+    }
+    </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Phantom Airlines</title>
 </head>
-<body>
-  
+<body style="background-color: gray;">
+
+    <h1 style="color:darkblue;">PHantom Airlines</h1>
+    <p>About Us</p> <p >About Us</p>
+    
+    
+    <br>
+    <br>
+  <img src="phantomairlinesbanner.png" alt="pic broke :(" width="100%">
+
 </body>
 </html>
