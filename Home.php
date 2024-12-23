4125983301be0a42547739f51fa5c5f25741d722
fid-ultra <fahadsunna2002@gmail.com>
2024-12-24 01:31:53 +0300

    first day of hard work

--- Home.php
+++ Home.php
@@ -2,11 +2,29 @@
 <html lang="en">
 <head>
     <style>
-    p{
-        display:inline-block;
-        color:darkblue;
-        text-align: left;
-    }
+   p{
+    display: inline;
+    color: darkblue;
+   font-size: 200%;
+padding: 10px;
+
+   }
+   p:hover{
+   background-color: silver;
+   transition: all 0.3s;
+   
+   }
+a{
+    text-decoration: none;
+}
+.uselessbanner{
+background-color: lightslategrey;
+position: fixed;
+bottom: 0;
+left:0;
+width:100%;
+}
+
     </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,12 +33,18 @@
 <body style="background-color: gray;">
 
     <h1 style="color:darkblue;">PHantom Airlines</h1>
-    <p>About Us</p> <p >About Us</p>
-    
+    <div style="text-align:right;">
+    <p><a href="booknow.php">Book Now!</a></p> <p ><a href="test.php">Flight status</a></p>
+    <P><a href="test.php"> Trips/ Check-in</a></P> <p><a href="test.php">About Us</a></p>
+    </div>
     
     <br>
     <br>
+    
   <img src="phantomairlinesbanner.png" alt="pic broke :(" width="100%">
-
+  <div class="text"><h2>Book your First flight see your flight status or just stalk us its all up to you :)</h2></div>
+  <div class="uselessbanner">
+   <h1> PHantom Airlines group: 202220123/202220632/202210102/202220005/202110662</h1>
+  </div>
 </body>
 </html>
